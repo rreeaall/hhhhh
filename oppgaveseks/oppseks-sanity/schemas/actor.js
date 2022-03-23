9cb7bcf4bb953b59a5dd70ab81bd360a6a5b3487
rreeaall <98414511+rreeaall@users.noreply.github.com>
2022-03-23 23:13:51 +0100

    Add files via upload

---
+++ oppgaveseks/oppseks-sanity/schemas/actor.js
@@ -0,0 +1,14 @@
+const actor = {
+    title: 'actor',
+    name: 'actor',
+    type: 'document',
+    fields: [
+    {
+    fullName: 'string',
+    name: 'slug',
+    type: 'slug',
+    }
+  ]
+}
+
+export default actor;
